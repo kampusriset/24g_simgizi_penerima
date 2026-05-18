8e20127b17471af2fffe5eceefc47c387b91cc0c
iKzuu <anangsetiaji30@gmail.com>
2026-05-18 21:34:48 +0700

    menambahkan SekolahController untuk menangani penyimpanan data sekolah

---
+++ controllers/SekolahController.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . "/../models/Sekolah.php";
+
+class SekolahController
+{
+    private Sekolah $model;
+
+    public function __construct()
+    {
+        $this->model = new Sekolah();
+    }
+
+    public function store()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $nama_sekolah =
+                $_POST['nama_sekolah'] ?? '';
+
+            $alamat =
+                $_POST['alamat_sekolah'] ?? '';
+
+            $jenjang =
+                $_POST['jenjang'] ?? '';
+
+            $this->model->create(
+                $nama_sekolah,
+                $alamat,
+                $jenjang
+            );
+
+            $newSchool =
+                $this->model->getLastInserted();
+
+            echo json_encode([
+                "success" => true,
+                "school" => $newSchool
+            ]);
+
+            exit;
+        }
+    }
+}
